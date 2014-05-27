ff7a11b1ec1de2c0f19aac74015339c0f19845ce
GuginoGaming <GuginoGaming@GuginoGaming-PC.home>
2014-05-26 20:45:01 -0400

    Added player login detection

--- src/me/Gugino/economy/EconomyPlus.java
+++ src/me/Gugino/economy/EconomyPlus.java
@@ -8,6 +8,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EconomyPlus extends JavaPlugin implements Listener{
@@ -25,6 +26,14 @@ public class EconomyPlus extends JavaPlugin implements Listener{
 		saveConfig();
 	}
 	
+	public void onPlayerJoin(PlayerJoinEvent e){
+		Player p = e.getPlayer();
+		
+		if(!(getConfig().contains(p.getName()))){
+			getConfig().set(p.getName() + ".balance", "" + 0);
+		}
+	}
+	
 	public boolean onCommand(CommandSender sender, Command commandd, String label, String[] args){
 		
 		Player player = (Player) sender;
@@ -33,6 +42,10 @@ public class EconomyPlus extends JavaPlugin implements Listener{
 			if(args.length == 0){
 				player.sendMessage(ChatColor.DARK_AQUA + "[EconomyPlus] " + ChatColor.AQUA + "Invalid Syntax: /ep-pay <player> <amount>");
 				return true;
+			}
+			if(args.length == 1){
+				Player targetPlayer = Bukkit.getServer().getPlayer(args[0]);
+				
 			}
 		}
 		
